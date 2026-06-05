2e6243ea6dccaded031896c53ec0627433053658
Veronica <anggrainiveronica227@gmail.com>
2026-06-05 16:07:25 +0700

    add page for welcome page

--- app/src/main/java/com/veroanggra/greetingremotecompose/MainActivity.kt
+++ app/src/main/java/com/veroanggra/greetingremotecompose/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.veroanggra.greetingremotecompose.remote.greeting_card.CardViewModel
+import com.veroanggra.greetingremotecompose.remote.greeting_card.RemoteWelcomeCardLoader
+import com.veroanggra.greetingremotecompose.remote.greeting_card.uploadRemoteLayout
 import com.veroanggra.greetingremotecompose.ui.theme.GreetingRemoteComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +33,61 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GreetingRemoteComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val cardViewModel: CardViewModel = viewModel()
+                val context = LocalContext.current
+                val scope = rememberCoroutineScope()
+                val isAdmin = false
+                var isRefreshing by remember { mutableStateOf(false) }
+
+                LaunchedEffect(Unit) {
+                    try {
+                        if (isAdmin) {
+                            uploadRemoteLayout(context) {
+                                cardViewModel.fetchRemoteBlueprint()
+                            }
+                        } else {
+                            cardViewModel.fetchRemoteBlueprint()
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(WindowInsets(0, 0, 0, 0))
+                ) {
+                    PullToRefreshBox(
+                        isRefreshing = isRefreshing,
+                        onRefresh = {
+                            scope.launch {
+                                isRefreshing = true
+                                try {
+                                    cardViewModel.fetchRemoteBlueprint()
+                                } catch (e: Exception) {
+                                    e.printStackTrace()
+                                } finally {
+                                    isRefreshing = false
+                                }
+                            }
+                        },
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            item {
+                                Box(
+                                    modifier = Modifier.fillParentMaxSize()
+                                ) {
+                                    RemoteWelcomeCardLoader(viewModel = cardViewModel)
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreetingRemoteComposeTheme {
-        Greeting("Android")
     }
 }
