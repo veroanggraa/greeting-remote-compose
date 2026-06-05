ad3445a0a1cf756f525a2c21009b1df8c067711b
Veronica <anggrainiveronica227@gmail.com>
2026-06-05 15:53:56 +0700

    add google service file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
     namespace = "com.veroanggra.greetingremotecompose"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.veroanggra.greetingremotecompose"
-        minSdk = 24
-        targetSdk = 36
+        minSdk = 23
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
@@ -48,6 +45,16 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.remote.core)
+    implementation(libs.remote.creation.android)
+    implementation(libs.remote.creation.compose)
+    implementation(libs.remote.player.core)
+    implementation(libs.remote.player.view)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.remote.material3)
+    implementation(libs.firebase.storage.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
